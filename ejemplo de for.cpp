b63df95b0ac1127912e3190993dc71a75fb80d69
Richardcruz2016 <richardcruz15@hotmail.com>
2016-04-27 16:51:43 -0600

    codigo para sacar promedio de 10 notas

--- ejemplo de for.cpp
+++ ejemplo de for.cpp
@@ -1,10 +1,25 @@
 #include<iostream>
 #include<stdlib.h>
-int numero[10]={ 1,2,3,4,5,8,9,1,4,6};
+int numero[10];
 main(){
 	using namespace std;
-	for(int i =0;i<10;i++){
-		cout<<numero[i]<<" ";	
+	cout<<"ingrese sus notas"<<endl;
+	for(int i=0;i<10;i++){
+		cout<<"ingrese nota "<<i+1<< " : ";
+		cin>>numero[i];	
 	}
 	cout<<endl;
+	float suma=0;
+	for(int i=0;i<10;i++){
+		suma=suma+numero[i];
+		
+	}
+	float promedio=suma/10;
+	cout<<"su promedio es "<<promedio<<endl;
+	
 }
+
+
+
+
+
